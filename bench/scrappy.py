bc9e4638c3035156d85f9c58e0ab978817563d44
William Espegren <131612909+WilliamEspegren@users.noreply.github.com>
2024-06-23 16:44:07 +0200

    chore(scrappy): add type hints to scrappy.py

--- bench/scrappy.py
+++ bench/scrappy.py
@@ -3,32 +3,35 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.crawler import CrawlerProcess
 from urllib.parse import urlparse
+from scrapy.settings import Settings
+from scrapy.http import Response
+from typing import List, Type, Union, ClassVar
 
-url = len(sys.argv) > 1 and str(sys.argv[1]) or "https://rsseau.fr"
-host = urlparse(url).hostname
+url: str = len(sys.argv) > 1 and str(sys.argv[1]) or "https://rsseau.fr"
+host: str = urlparse(url).hostname
 
 class MySpider(CrawlSpider):
-    name = host
-    allowed_domains = [host]
-    start_urls = [url]
-    links = []
-    rules = (
+    name: Union[str, None] = host
+    allowed_domains: List[str] = [host]
+    start_urls: List[str] = [url]
+    links: List[str] = []
+    rules: ClassVar[tuple] = (
         Rule(LinkExtractor(), callback='parse_item', follow=True),
     )
 
     @classmethod
-    def update_settings(cls, settings):
+    def update_settings(cls: Type['MySpider'], settings: Settings) -> None:
         super().update_settings(settings)
         settings.set("LOG_ENABLED", "false", priority="spider")
 
-    def parse_item(self, response):
+    def parse_item(self, response: Response) -> None:
         self.links.append(response.url)
 
 print("benching scrappy(python)...")
-process = CrawlerProcess()
-spider = MySpider
-start = time.time()
+process: CrawlerProcess = CrawlerProcess()
+spider: Type[MySpider] = MySpider
+start: float = time.time()
 process.crawl(spider)
 process.start()
-end = time.time()
-print(url, "pages found " + str(len(spider.links)), "elasped duration " + str(end - start) + "s", sep="\n")
+end: float = time.time()
+print(url, "pages found " + str(len(spider.links)), "elapsed duration " + str(end - start) + "s", sep="\n")
