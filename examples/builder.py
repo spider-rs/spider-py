a423fabf708607ec16e95d1150f81a60697118f8
William Espegren <131612909+WilliamEspegren@users.noreply.github.com>
2024-06-23 17:06:09 +0200

    chore(builder): add type hints to builder.py

--- examples/builder.py
+++ examples/builder.py
@@ -1,15 +1,14 @@
 import asyncio
-
+from typing import List
 from spider_rs import Website
 
-async def main():
-    website = (
+async def main() -> None:
+    website: Website = (
         Website("https://choosealicense.com", False)
         .with_user_agent("BotBot")
         .with_headers({"authorization": "Something "})
     )
     website.crawl()
     print(website.get_links())
-
 
 asyncio.run(main())
